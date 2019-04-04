54297e246507c4f60df3ecb86cb323863cedeabb
Mark Bennett <mark.bennett@ucf.edu>
2019-04-04 10:30:23 -0400

    update css

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );// add parent style
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-   wp_enqueue_style( 'post-style', get_stylesheet_directory_uri() .'/includes/css/provost-news-post.css' );
+   wp_enqueue_style( 'post-style', get_stylesheet_directory_uri() .'/includes/css/provost-news-post.css', array(),'1.0.0' );
 }
 
 require_once( PROVOST_NEWS_THEME_DIR . 'template-parts/header/news_header.php');
